b3c672714f184c74b2cad9af3378583b0f6ac43c
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-06-28 20:44:00 +0300

    feat: add Main

--- src/main/java/gr/aueb/cf/final_project_CF7/FinalProjectCf7Application.java
+++ src/main/java/gr/aueb/cf/final_project_CF7/FinalProjectCf7Application.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.final_project_CF7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinalProjectCf7Application {
 
 	public static void main(String[] args) {
