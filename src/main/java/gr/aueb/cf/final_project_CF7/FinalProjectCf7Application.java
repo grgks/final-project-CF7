afdec010ba74cc28f8bee449ab5d6b8f467ec214
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-06-28 20:14:57 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/final_project_CF7/FinalProjectCf7Application.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.final_project_CF7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCf7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCf7Application.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/final_project_CF7/FinalProjectCf7ApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.final_project_CF7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectCf7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
